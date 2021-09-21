5dd86e8e892fa697ecce9143726d18a251778757
anurag mishra <anurag171@gmail.com>
2021-09-21 11:35:35 +0530

    added a consumer

--- kafkademo/src/main/java/com/anurag/kafka/consumer/KafkaConsumerDemo.java
+++ kafkademo/src/main/java/com/anurag/kafka/consumer/KafkaConsumerDemo.java
@@ -1,9 +1,13 @@
 package com.anurag.kafka.consumer;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class KafkaConsumerDemo {
 
 	public static void main(String[] args) {
 		String[] topics = new String[] {"my_first_iot_topic","second_topic"};
+				
 		ConsumerHelper.subscribe(ConsumerHelper.createConsumer(ConsumerHelper.init()), topics);
 		
 
